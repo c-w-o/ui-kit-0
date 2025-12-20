7771236ba16c5a0efa410212e92626cd1c21359d
cwo <github2@chwolf.net>
2025-12-20 23:30:30 +0100

    added table

--- src/ui-kit-0.js
+++ src/ui-kit-0.js
@@ -1,6 +1,7 @@
 export * from "./ui/base.js";
 export * from "./ui/layout.js";
 export * from "./ui/controls.js";
+export * from "./ui/table.js";
 export * from "./ui/json-editor.js";
 export * from "./ui/chart.js";
 export * from "./ui/validation.js";
